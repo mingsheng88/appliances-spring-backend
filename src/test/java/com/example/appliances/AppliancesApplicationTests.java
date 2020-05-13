b51e0a8aa4532da975771d744a72d3273dbdd502
Mingsheng Gan <mingsheng88@hotmail.com>
2020-05-13 11:47:22 +0800

    Setup project using spring initializr

---
+++ src/test/java/com/example/appliances/AppliancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appliances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
