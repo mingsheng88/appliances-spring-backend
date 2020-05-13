dd1a44937f49eac4f0d29da57fac3f05e06c596e
Mingsheng Gan <mingsheng88@hotmail.com>
2020-05-13 11:49:15 +0800

    Setup basic model for Appliance

---
+++ src/main/java/com/example/appliances/models/Appliance.java
@@ -0,0 +1,78 @@
+package com.example.appliances.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.sql.Date;
+import java.util.UUID;
+
+public class Appliance {
+    // TODO: Add validators
+    private UUID id;
+    private String serialNo;
+    private String brand;
+    private String model;
+    private String status;
+    private Date purchaseDate;
+
+    public Appliance(@JsonProperty("id") UUID id,
+                     @JsonProperty("serialNo") String serialNo,
+                     @JsonProperty("brand") String brand,
+                     @JsonProperty("model") String model,
+                     @JsonProperty("status") String status,
+                     @JsonProperty("purchaseDate") String purchaseDate) {
+        this.id = id;
+        this.serialNo = serialNo;
+        this.brand = brand;
+        this.model = model;
+        this.status = status;
+        this.purchaseDate = Date.valueOf(purchaseDate);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+}
